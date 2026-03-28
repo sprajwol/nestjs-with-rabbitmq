0e3a720be4c628dad5ab9a9d276c7b899b3205ba
Prajwol Man Shakya <shakyaprajwol96@gmail.com>
2026-03-28 21:16:12 +0545

    env read setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { RabbitmqDirectExchangeModule } from './modules/rabbitmqDirectExchange/rabbitmqDirectExchange.module';
 
 @Module({
-  imports: [RabbitmqDirectExchangeModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    RabbitmqDirectExchangeModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger('Main');
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+
+  const appPort = configService.get<number>('APP_PORT') || 3000;
+
+  //enable cors
+  app.enableCors();
+
+  await app.listen(appPort);
+  logger.log(`Application started at port: ${appPort}`);
 }
+
 bootstrap();
