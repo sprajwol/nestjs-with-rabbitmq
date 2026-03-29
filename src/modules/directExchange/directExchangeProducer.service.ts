2b9f651e7ed50cc5cc53297fbee1a98465770f6d
Prajwol Man Shakya <shakyaprajwol96@gmail.com>
2026-03-29 16:01:05 +0545

    update direct exchange producer  to work as child class of base producer

--- src/modules/directExchange/directExchangeProducer.service.ts
+++ src/modules/directExchange/directExchangeProducer.service.ts
@@ -1,64 +1,42 @@
 import { Inject, Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import {
-  type AmqpConnectionManager,
-  ChannelWrapper,
-  connect,
-} from 'amqp-connection-manager';
+import { type AmqpConnectionManager } from 'amqp-connection-manager';
 import { Channel, ConfirmChannel } from 'amqplib';
+import { RabbitmqBaseProducer } from 'src/common/integrations/rabbitmq/rabbitmq-baseProducer';
 import { RABBITMQ_CONNECTION } from 'src/common/integrations/rabbitmq/rabbitmq.constants';
 
 @Injectable()
-export class DirectExchangeProducerService {
-  // private connection: amqp.AmqpConnectionManager;
-  // private channelModel: amqp.ChannelModel;
+export class DirectExchangeProducerService extends RabbitmqBaseProducer {
+  protected readonly logger = new Logger(DirectExchangeProducerService.name);
 
-  // private connection: AmqpConnectionManager;
-  private channelWrapper: ChannelWrapper;
-
-  private readonly logger = new Logger(DirectExchangeProducerService.name);
-  
   private readonly rabbitmqDirectExchangeName: string; // RabbitMQ rabbitmqDirectExchangeName
   private readonly rabbitmqDirectExchangeQueueName: string; // RabbitMQ rabbitmqDirectExchangeQueueName
   private readonly rabbitmqDirectRoutingKey: string; // RabbitMQ rabbitmqDirectRoutingKey
 
   constructor(
+    @Inject(RABBITMQ_CONNECTION) connection: AmqpConnectionManager,
     private readonly configService: ConfigService,
-    @Inject(RABBITMQ_CONNECTION) private readonly connection: AmqpConnectionManager
   ) {
+    super(connection);
+
     this.rabbitmqDirectExchangeName = this.configService.getOrThrow<string>('RABBITMQ_DIRECT_EXCHANGE_NAME');
     this.rabbitmqDirectExchangeQueueName = this.configService.getOrThrow<string>('RABBITMQ_DIRECT_EXCHANGE_QUEUE_NAME');
     this.rabbitmqDirectRoutingKey = this.configService.getOrThrow<string>('RABBITMQ_DIRECT_ROUTING_KEY');
   }
 
-  async onModuleInit() {
-    await this.initConnection();
-  }
+  protected async setupChannel(channel: ConfirmChannel): Promise<void> {
+    await channel.assertExchange(this.rabbitmqDirectExchangeName, 'direct', { durable: false });
+    await channel.assertQueue(this.rabbitmqDirectExchangeQueueName, { durable: false });
+    await channel.bindQueue( this.rabbitmqDirectExchangeQueueName, this.rabbitmqDirectExchangeName, this.rabbitmqDirectRoutingKey);
 
-  async initConnection() {
-    this.channelWrapper = this.connection.createChannel({
-      json: true,
-      setup: async (channel: ConfirmChannel) => {
-        await channel.assertExchange(this.rabbitmqDirectExchangeName, 'direct' , { durable: false });
-        await channel.assertQueue(this.rabbitmqDirectExchangeQueueName, { durable: false });
-        await channel.bindQueue(this.rabbitmqDirectExchangeQueueName, this.rabbitmqDirectExchangeName, this.rabbitmqDirectRoutingKey);
-      }
-    });
-
-    this.channelWrapper.on('error', (error) => {
-      this.logger.error(`Channel error. ${error}`);
-    });
-
-    this.channelWrapper.on('close', () => {
-      this.logger.warn(`Channel closed.`);
-    });
-
-    this.channelWrapper.on('return', (msg) => {
-      this.logger.warn(`Message Returned (Unroutable): ${JSON.stringify(msg)}`);
-    });
-
-    this.channelWrapper.on('drain', () => {
-      this.logger.warn(`Channel drained.  Backpressure relieved, can resume sending messages.`);
-    });
+    this.logger.log(
+      `
+        Channel Setup Completed:
+        Exchange '${this.rabbitmqDirectExchangeName}',
+        Type: 'direct'',
+        Queue: '${this.rabbitmqDirectExchangeQueueName}',
+        RoutingKey: '${this.rabbitmqDirectRoutingKey}',
+      `,
+    );
   }
 }
