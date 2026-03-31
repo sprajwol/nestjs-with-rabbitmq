498ab0a6a8ccf82bdf52144b60a8badf9400fcb9
Prajwol Man Shakya <shakyaprajwol96@gmail.com>
2026-03-31 17:06:29 +0545

    add DLX DLQ config

--- src/modules/direct-exchange/direct-exchange.producer.service.ts
+++ src/modules/direct-exchange/direct-exchange.producer.service.ts
@@ -27,9 +27,25 @@ export class DirectExchangeProducerService extends RabbitmqBaseProducer {
 
   protected async setupChannel(channel: ConfirmChannel): Promise<void> {
     try {
+      // Dead Letter Exchange(DLX) and Dead Letter Queue(DLQ) setup for handling failed messages in separate queue.
+      const dlxName =  `${this.rabbitmqDirectExchangeName}.dlx`;
+      const dlqName =  `${this.rabbitmqDirectExchangeQueueName}.dlq`;
+      await channel.assertExchange(dlxName, 'direct', { durable: true });
+      await channel.assertQueue(dlqName, { durable: true });
+      await channel.bindQueue(dlqName, dlxName, this.rabbitmqDirectRoutingKey);
+
+      // Main exchange and queue setup with dead letter configuration.
       await channel.assertExchange(this.rabbitmqDirectExchangeName, 'direct', { durable: true });
-      await channel.assertQueue(this.rabbitmqDirectExchangeQueueName, { durable: true });
-      await channel.bindQueue( this.rabbitmqDirectExchangeQueueName, this.rabbitmqDirectExchangeName, this.rabbitmqDirectRoutingKey);
+      await channel.assertQueue(
+        this.rabbitmqDirectExchangeQueueName,
+        {
+          durable: true,
+          arguments: {
+            'x-dead-letter-exchange': dlxName,
+            'x-dead-letter-routing-key': this.rabbitmqDirectRoutingKey,
+          }
+        });
+      await channel.bindQueue(this.rabbitmqDirectExchangeQueueName, this.rabbitmqDirectExchangeName, this.rabbitmqDirectRoutingKey);
 
       this.logger.log(
         `
