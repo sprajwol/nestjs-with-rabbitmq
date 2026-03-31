e892028d5a8d19cb87724f707da3d2e9d8874866
Prajwol Man Shakya <shakyaprajwol96@gmail.com>
2026-03-31 12:34:04 +0545

    mvoe publishToQueue to base produver

--- src/common/integrations/rabbitmq/rabbitmq-baseProducer.ts
+++ src/common/integrations/rabbitmq/rabbitmq-baseProducer.ts
@@ -3,7 +3,7 @@ import {
   type AmqpConnectionManager,
   ChannelWrapper,
 } from 'amqp-connection-manager';
-import { Channel, ConfirmChannel } from 'amqplib';
+import { Channel, ConfirmChannel, Options } from 'amqplib';
 import { RABBITMQ_CONNECTION } from './rabbitmq.constants';
 
 export abstract class RabbitmqBaseProducer implements OnModuleInit, OnModuleDestroy {
@@ -60,4 +60,43 @@ export abstract class RabbitmqBaseProducer implements OnModuleInit, OnModuleDest
   // Child classes must implement the setupChannel method to create and configure the channel as needed (E.g. asserting exchanges, queues, bindings, etc.)
   // 'abstract' keyword forces the child class to implement this method, ensuring that the necessary channel is properly setup up for each child instance producer to properly function.
   protected abstract setupChannel(channel: ConfirmChannel): Promise<void>;
+
+  protected async publishToQueue(message: any, messageId: string, exchangeName: string,  routingKey: string) {
+    if (!this.connection.isConnected()) {
+      this.logger.error(`Cannot publish message. RabbitMQ is  not connected.`);
+
+      throw new Error(`RabbitMQ connection is not established.`);
+    }
+
+    const timestamp = Date.now();
+    try {
+      const buffer = Buffer.from(JSON.stringify(message));
+      //  `persistent: true` makes the message persistent(saved to disk) if true or non-persistent(stored to memory) if false.
+      //  Persistent messages survive broker(rabbitmq) restarts.
+      //  Needs 'exchange' and 'queue' also to be durable to ensure message persistence.
+
+      //  `mandatory: true` ensures that if a message cannot be routed to any queue (e.g. incorrect routing key), the message will be returned to the producer instead of being silently dropped.
+      //  This allows the producer to handle unroutable messages as needed (e.g. logging, retryiing, etc.) and prevent message loss due to misconfigurtation or other issues.
+      const publishOptions: Options.Publish = {
+        messageId: messageId,
+        timestamp: timestamp,
+        persistent: true,
+        mandatory: true,
+        contentType: 'application/json',
+      };
+      
+      await this.channelWrapper.publish(
+        exchangeName,
+        routingKey,
+        buffer,
+        publishOptions,
+      );
+
+      this.logger.log(`Message with ID '${messageId}' has been published to exchange '${exchangeName}' with routing key '${routingKey}'.`);
+    } catch (error) {
+      this.logger.error(`Failed to publish message with ID ${messageId}: ${error}`);
+
+      throw error;
+    }
+  }
 }
