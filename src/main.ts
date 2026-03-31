8ac33523081663feaef8c830c6b233620106fbb3
Prajwol Man Shakya <shakyaprajwol96@gmail.com>
2026-03-31 11:24:07 +0545

    configured swagger

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import { Logger } from '@nestjs/common';
+import { INestApplication, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const logger = new Logger('Main');
@@ -13,8 +14,33 @@ async function bootstrap() {
   //enable cors
   app.enableCors();
 
+  configureSwagger(app);
+
   await app.listen(appPort);
   logger.log(`Application started at port: ${appPort}`);
 }
 
 bootstrap();
+
+function configureSwagger(app: INestApplication): void {
+  const config = new  DocumentBuilder()
+    .setTitle('NestJS with RabbitMQ')
+    .setDescription('A NestJS application to demonstrate usage of RabbbitMQ in various ways.')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app,  document, {
+    swaggerOptions: {
+      tagsSorter: 'alpha',
+      opeationsSorter: 'alpha',
+      docExpansion: 'none',
+      defaultModelsExpandDepth: 5,
+      persistAuthorization: true,
+      displayRequestDuration: true,
+      filter: true,
+    }
+  })
+}
