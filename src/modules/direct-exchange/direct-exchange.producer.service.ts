2ad085298fa23bc08b7fcc62ace7226aa68d384a
Prajwol Man Shakya <shakyaprajwol96@gmail.com>
2026-04-03 12:52:18 +0545

    import update

--- src/modules/direct-exchange/direct-exchange.producer.service.ts
+++ src/modules/direct-exchange/direct-exchange.producer.service.ts
@@ -2,8 +2,8 @@ import { Inject, Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { type AmqpConnectionManager } from 'amqp-connection-manager';
 import { Channel, ConfirmChannel, Options } from 'amqplib';
-import { RabbitmqBaseProducer } from 'src/common/integrations/rabbitmq/rabbitmq.base-producer';
-import { RABBITMQ_CONNECTION } from 'src/common/integrations/rabbitmq/rabbitmq.constants';
+import { RabbitmqBaseProducer } from '../../common/integrations/rabbitmq/rabbitmq.base-producer';
+import { RABBITMQ_CONNECTION } from '../../common/integrations/rabbitmq/rabbitmq.constants';
 import { QueuePayloadDto } from './dtos/queue-payload.dto';
 
 @Injectable()
@@ -67,5 +67,4 @@ export class DirectExchangeProducerService extends RabbitmqBaseProducer {
   async processMessage(message: QueuePayloadDto, messageId: string) {
     return await this.publishToQueue<QueuePayloadDto>(message, messageId, this.rabbitmqDirectExchangeName, this.rabbitmqDirectRoutingKey);
   }
-
 }
