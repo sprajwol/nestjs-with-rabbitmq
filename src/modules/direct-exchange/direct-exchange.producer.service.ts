4a89b8e33f9aa0875833ce61c4657b40a6baea46
Prajwol Man Shakya <shakyaprajwol96@gmail.com>
2026-04-04 22:18:47 +0545

    queues assert and bindings update

--- src/modules/direct-exchange/direct-exchange.producer.service.ts
+++ src/modules/direct-exchange/direct-exchange.producer.service.ts
@@ -11,9 +11,15 @@ import { QueuePayloadDto } from '#src/modules/direct-exchange/dtos/queue-payload
 export class DirectExchangeProducerService extends RabbitmqBaseProducer {
   protected readonly logger = new Logger(this.constructor.name);
 
-  private readonly rabbitmqDirectExchangeName: string; // RabbitMQ rabbitmqDirectExchangeName
-  private readonly rabbitmqDirectExchangeQueueName: string; // RabbitMQ rabbitmqDirectExchangeQueueName
-  private readonly rabbitmqDirectRoutingKey: string; // RabbitMQ rabbitmqDirectRoutingKey
+  private readonly main_exchange: string; // RabbitMQ rabbitmqDirectExchangeName
+  private readonly main_queue: string; // RabbitMQ rabbitmqDirectExchangeQueueName
+  private readonly main_routing_key: string; // RabbitMQ rabbitmqDirectRoutingKey
+
+  private readonly retry_queue: string; // RabbitMQ retry queue name
+  private readonly retry_routing_key: string; // RabbitMQ retry routing key
+
+  private readonly parking_queue: string; // RabbitMQ parking queue name
+  private readonly parking_routing_key: string; // RabbitMQ parking routing key
 
   constructor(
     @Inject(RABBITMQ_CONNECTION) connection: AmqpConnectionManager,
@@ -21,40 +27,74 @@ export class DirectExchangeProducerService extends RabbitmqBaseProducer {
   ) {
     super(connection);
 
-    this.rabbitmqDirectExchangeName = this.configService.getOrThrow<string>('RABBITMQ_DIRECT_EXCHANGE_NAME');
-    this.rabbitmqDirectExchangeQueueName = this.configService.getOrThrow<string>('RABBITMQ_DIRECT_EXCHANGE_QUEUE_NAME');
-    this.rabbitmqDirectRoutingKey = this.configService.getOrThrow<string>('RABBITMQ_DIRECT_ROUTING_KEY');
+    this.main_exchange = this.configService.getOrThrow<string>('RABBITMQ_DIRECT_EXCHANGE_NAME');
+
+    this.main_queue = this.configService.getOrThrow<string>('RABBITMQ_DIRECT_EXCHANGE_QUEUE_NAME');
+    this.main_routing_key = this.configService.getOrThrow<string>('RABBITMQ_DIRECT_ROUTING_KEY');
+
+    this.retry_queue = `${this.main_queue}_retry`;
+    this.retry_routing_key = `${this.main_routing_key}_retry`;
+
+    this.parking_queue = `${this.main_queue}_parking`;
+    this.parking_routing_key = `${this.main_routing_key}_retry`;
   }
 
   protected async setupChannel(channel: ConfirmChannel): Promise<void> {
     try {
       // Dead Letter Exchange(DLX) and Dead Letter Queue(DLQ) setup for handling failed messages in separate queue.
-      const dlxName = `${this.rabbitmqDirectExchangeName}.dlx`;
-      const dlqName = `${this.rabbitmqDirectExchangeQueueName}.dlq`;
-      await channel.assertExchange(dlxName, 'direct', { durable: true });
-      await channel.assertQueue(dlqName, { durable: true });
-      await channel.bindQueue(dlqName, dlxName, this.rabbitmqDirectRoutingKey);
+      
+      await channel.assertExchange(this.main_exchange, 'direct', { durable: true });
 
-      // Main exchange and queue setup with dead letter configuration.
-      await channel.assertExchange(this.rabbitmqDirectExchangeName, 'direct', { durable: true });
-      await channel.assertQueue(
-        this.rabbitmqDirectExchangeQueueName,
-        {
-          durable: true,
-          arguments: {
-            'x-dead-letter-exchange': dlxName,
-            'x-dead-letter-routing-key': this.rabbitmqDirectRoutingKey,
-          }
-        });
-      await channel.bindQueue(this.rabbitmqDirectExchangeQueueName, this.rabbitmqDirectExchangeName, this.rabbitmqDirectRoutingKey);
+      await channel.assertQueue(this.main_queue, {
+        durable: true,
+        arguments: {
+          'x-dead-letter-exchange': this.main_exchange,
+          'x-dead-letter-routing-key': this.retry_routing_key,
+        },
+      });
+
+      await channel.assertQueue(this.retry_queue, {
+        durable: true,
+        arguments: {
+          'x-message-ttl': 10000,
+          'x-dead-letter-exchange': this.main_exchange,
+          'x-dead-letter-routing-key': this.main_routing_key,
+        },
+      });
+
+      await channel.assertQueue(this.parking_queue, {
+        durable: true,
+      });
+
+      await channel.bindQueue(this.main_queue, this.main_exchange, this.main_routing_key);
+      await channel.bindQueue(this.retry_queue, this.main_exchange, `${this.retry_routing_key}`);
+      await channel.bindQueue(this.parking_queue, this.main_exchange, `${this.parking_routing_key}`);
 
       this.logger.log(
         `
-          Channel Setup Completed:
-          Exchange '${this.rabbitmqDirectExchangeName}',
+          Main Queue Setup Completed:
+          Exchange '${this.main_exchange}',
           Type: 'direct',
-          Queue: '${this.rabbitmqDirectExchangeQueueName}',
-          RoutingKey: '${this.rabbitmqDirectRoutingKey}',
+          Queue: '${this.main_queue}',
+          RoutingKey: '${this.main_routing_key}',
+        `,
+      );
+      this.logger.log(
+        `
+          Retry Queue Setup Completed:
+          Exchange '${this.main_exchange}',
+          Type: 'direct',
+          Queue: '${this.retry_queue}',
+          RoutingKey: '${this.retry_routing_key}',
+        `,
+      );
+      this.logger.log(
+        `
+          Parking Queue Setup Completed:
+          Exchange '${this.main_exchange}',
+          Type: 'direct',
+          Queue: '${this.parking_queue}',
+          RoutingKey: '${this.parking_routing_key}',
         `,
       );
     } catch (error) {
@@ -66,6 +106,6 @@ export class DirectExchangeProducerService extends RabbitmqBaseProducer {
   }
 
   async processMessage(message: QueuePayloadDto, messageId: string) {
-    return await this.publishToQueue<QueuePayloadDto>(message, messageId, this.rabbitmqDirectExchangeName, this.rabbitmqDirectRoutingKey);
+    return await this.publishToQueue<QueuePayloadDto>(message, messageId, this.main_exchange, this.main_routing_key);
   }
 }
